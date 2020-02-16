8e4a7e05e21d5b69339ddaaca59287f218c74d5e
SamuelHiroyuki <samduasvezesmortal@gmail.com>
2020-02-16 12:28:30 -0300

    Inicio da sidebar

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import GlobalStyle from "./styles/global";
 import theme from "./styles/theme";
 
 import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
 
 function App() {
 	return (
 		<ThemeProvider theme={theme}>
 			<Header />
+			<Sidebar />
 			<GlobalStyle />
 		</ThemeProvider>
 	);
