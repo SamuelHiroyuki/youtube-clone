9b14ddf9c6589857a6172a90f937fa9fcddc3935
SamuelHiroyuki <samduasvezesmortal@gmail.com>
2020-02-16 00:16:03 -0300

    Finalização do Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,14 @@
 import React from "react";
-import { IoIosMenu } from "react-icons/io";
+import { IoIosMenu, IoMdSearch } from "react-icons/io";
+import { GoDeviceCameraVideo } from "react-icons/go";
+import { MdApps } from "react-icons/md";
+import { AiFillBell } from "react-icons/ai";
+import { FaKeyboard } from "react-icons/fa";
 
 import { ReactComponent as Logo } from "../../assets/svgs/logo.svg";
+import avatar from "../../assets/images/avatar.png";
 
-import { Container, Button, LogoConteiner } from "./styles";
+import { Container, Button, LogoConteiner, Avatar, Badge } from "./styles";
 
 export default function Header() {
 	return (
@@ -16,6 +21,29 @@ export default function Header() {
 					<Logo />
 					<span>BR</span>
 				</LogoConteiner>
+			</div>
+
+			<form action="">
+				<input type="text" placeholder="Pesquisar" />
+				<FaKeyboard size={16} color="#333" />
+				<button type="button">
+					<IoMdSearch size={20} color="#333" />
+				</button>
+			</form>
+
+			<div>
+				<Button width={40} flex>
+					<GoDeviceCameraVideo size={16} color="#606060" />
+				</Button>
+				<Button width={40} flex>
+					<MdApps size={24} color="#606060" />
+				</Button>
+				<Button width={40} flex>
+					<AiFillBell size={22} color="#606060" />
+					<Badge>9+</Badge>
+				</Button>
+
+				<Avatar src={avatar} />
 			</div>
 		</Container>
 	);
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,12 +1,11 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const Container = styled.div`
 	height: 56px;
 	padding: 0 16px;
-	background-color: #f00;
 
 	display: flex;
-	justify-content: space-between;
+	justify-content: flex-start;
 	align-items: center;
 
 	& > div {
@@ -14,15 +13,74 @@ export const Container = styled.div`
 		justify-content: space-between;
 		align-items: center;
 	}
+
+	& form {
+		position: relative;
+		width: 100%;
+		height: 30px;
+		display: flex;
+		align-items: center;
+		margin: 0 50px;
+
+		input {
+			width: 100%;
+			height: 30px;
+			border-radius: 2px 0 0 2px;
+			border: 1px solid #d3d3d3;
+			border-right: none;
+
+			::placeholder {
+				font-size: 16px;
+				font-weight: 400;
+				padding-left: 6px;
+			}
+		}
+
+		& input + svg {
+			position: absolute;
+			right: 80px;
+		}
+
+		button {
+			width: 75px;
+			height: 30px;
+			border: 1px solid #d3d3d3;
+			padding: 1px 5px;
+			border-radius: 0 2px 2px 0;
+			cursor: pointer;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+
+			svg {
+				opacity: 0.6;
+			}
+
+			&:hover {
+				svg {
+					opacity: 1;
+				}
+			}
+		}
+	}
 `;
 
 export const Button = styled.button`
-	width: auto;
+	width: ${({ width }) => `${width}px` || "auto"};
 	height: 25px;
 	border: none;
 	margin: 5px;
-
+	position: relative;
+	background-color: transparent;
 	cursor: pointer;
+
+	${({ flex }) =>
+		flex &&
+		css`
+			display: flex;
+			justify-content: center;
+			align-items: center;
+		`}
 `;
 
 export const LogoConteiner = styled.div`
@@ -31,9 +89,33 @@ export const LogoConteiner = styled.div`
 	position: relative;
 
 	& span {
+		color: #606060;
 		position: absolute;
 		top: 0;
 		right: 4px;
 		font-size: 10px;
 	}
+`;
+
+export const Avatar = styled.img`
+	width: 32px;
+	height: 32px;
+	border-radius: 50%;
+	margin: 0 24px 0 14px;
+	cursor: pointer;
+`;
+
+export const Badge = styled.div`
+	width: 18px;
+	height: 18px;
+	border-radius: 50%;
+	background-color: ${props => props.theme.primary};
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	color: #fff;
+	font-size: 10px;
+	position: absolute;
+	top: -5px;
+	right: -1px;
 `;
